42fc102f3c3dd7c86f5c5fe00ffa9c585edad365
André Prata <andreprata@ua.pt>
2011-04-02 15:42:09 +0100

    Reproduce only in between campaigns

--- src/pt/ua/sd/shoal/TShoal.java
+++ src/pt/ua/sd/shoal/TShoal.java
@@ -247,6 +247,7 @@ public class TShoal extends Thread {
 	 */
 	protected void seasonEnd() {
 		changeState(INTERNAL_STATE_SCHOOL.spawning);
+		spawn();
 		monitor.releaseRemainers();
 
 		for (IDirOperShoal d : diropers) {
