b86beb21ff3954c3ee60f97e01204909831385c0
unknown <HAWKS@.(none)>
2011-04-11 14:34:10 +0100

    Client/Server initial commit

---
+++ src/pt/ua/sd/boat/network/BoatProtocolMessage.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pt.ua.sd.boat.network;
+
+/**
+ *
+ * @author Eriksson Monteiro <eriksson.monteiro@ua.pt>
+ */
+public class BoatProtocolMessage {
+
+}
---
+++ src/pt/ua/sd/log/network/LogServer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pt.ua.sd.log.network;
+
+/**
+ *
+ * @author Eriksson Monteiro <eriksson.monteiro@ua.pt>
+ */
+public class LogServer {
+
+}
