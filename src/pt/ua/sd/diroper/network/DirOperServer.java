b21cefc4590d662ff8d10f0e3e2e2ea7f066c009
André Prata <andreprata@ua.pt>
2011-04-20 23:06:40 +0100

    another small refactoring

--- src/pt/ua/sd/diroper/network/DirOperServer.java
+++ src/pt/ua/sd/diroper/network/DirOperServer.java
@@ -1,12 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * 
  */
 package pt.ua.sd.diroper.network;
 
 /**
- *
  * @author Eriksson Monteiro <eriksson.monteiro@ua.pt>
+ * @author André Prata <andreprata@ua.pt>
  */
 public class DirOperServer {
 }
