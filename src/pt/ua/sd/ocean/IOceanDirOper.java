136af3e3919d50c8993019b10e6cc72c171c6413
André Prata <andreprata@ua.pt>
2011-04-22 14:14:06 +0100

    removed unused methods

--- src/pt/ua/sd/ocean/IOceanDirOper.java
+++ src/pt/ua/sd/ocean/IOceanDirOper.java
@@ -3,9 +3,6 @@
  */
 package pt.ua.sd.ocean;
 
-import java.util.List;
-
-import pt.ua.sd.boat.BoatStats;
 import pt.ua.sd.diroper.DirOperId;
 import pt.ua.sd.diroper.DirOperStats.INTERNAL_STATE_DIROPER;
 
@@ -15,15 +12,6 @@ import pt.ua.sd.diroper.DirOperStats.INTERNAL_STATE_DIROPER;
  * 
  */
 public interface IOceanDirOper {
-
-	/**
-	 * Get all boat statistics for a company's boats.
-	 * 
-	 * @param companyId
-	 *            the company.
-	 * @return a list with the stats of boats from the given company.
-	 */
-	public List<BoatStats> getBoats(int companyId);
 	
 	/**
 	 * Set the DirOperState for a current DirOper.
