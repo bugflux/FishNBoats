b289057ca932df49226094201b2b62878271dd6a
André Prata <andreprata@ua.pt>
2011-04-22 15:41:01 +0100

    added outline for ocean protocol messages

--- src/pt/ua/sd/ocean/network/OceanProtocolMessage.java
+++ src/pt/ua/sd/ocean/network/OceanProtocolMessage.java
@@ -3,10 +3,30 @@
  */
 package pt.ua.sd.ocean.network;
 
+import pt.ua.sd.communication.toocean.OceanMessage;
 
 /**
- * @author Eriksson Monteiro <eriksson.monteiro@ua.pt>
  * @author André Prata <andreprata@ua.pt>
+ * @author Eriksson Monteiro <eriksson.monteiro@ua.pt>
  */
 public class OceanProtocolMessage {
+
+	protected OceanMessage m;
+
+	/**
+	 * Construct a new OceanMessage for Ocean Operations
+	 * 
+	 * @param m
+	 *            the message that describes the operation
+	 */
+	public OceanProtocolMessage(OceanMessage m) {
+		this.m = m;
+	}
+
+	/**
+	 * @return the m
+	 */
+	public OceanMessage getMessage() {
+		return m;
+	}
 }
