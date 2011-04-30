2054833d55b70e569c7e64c8dd363cf9259a6055
André Prata <andreprata@ua.pt>
2011-04-30 11:11:09 +0100

    forgot to add package-info files in the last commit

---
+++ src/pt/ua/sd/communication/todiroper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Messages used to/by the DirOper go in this package.
+ */
+package pt.ua.sd.communication.todiroper;
---
+++ src/pt/ua/sd/ocean/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package describe the Ocean monitor, where all Boats
+ * and Shoals move, and update their states, along with the DirOpers.
+ */
+package pt.ua.sd.ocean;
